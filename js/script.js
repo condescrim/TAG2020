35712fbbe17537b45cd1d1f86a2fdb70672f0731
Adrian <adrian.medina.barrientos@est.una.ac.cr>
2020-07-19 11:20:46 -0600

    .

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//map
+var mymap = L.map('mapid').setView([9.917363, -84.102550], 16);
+var marker = L.marker([9.917363, -84.102550]).addTo(mymap);
+marker.bindPopup("<b>Taller de Artesanías German</b>").openPopup();
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+//map
